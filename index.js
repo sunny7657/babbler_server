5f9e86db9e85a2e25974199c12179b5c18ca70ef
sunny7657 <ksvetlana466@gmail.com>
2024-08-22 22:30:11 +0200

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const { PORT = 3001, DATABASE_URL } = process.env;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
