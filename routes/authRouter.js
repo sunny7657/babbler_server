fd6fdfecbde8a624c346488bd6bec25aa494cf90
sunny7657 <ksvetlana466@gmail.com>
2024-10-10 22:30:18 +0200

    upd

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,18 @@
 import express from "express";
+import validateBody from "../decorators/validateBody";
+import { userSignupSchema } from "../schemas/userSchemas";
+import AuthControllers from "../controllers/authControllers";
 
 const authRouter = express.Router();
 
 authRouter.get("/", (req, res) => {
   res.send(res.json);
 });
+
+authRouter.post(
+  "/signup",
+  validateBody(userSignupSchema),
+  AuthControllers.signup
+);
 
 export default authRouter;
--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 import { emailRegex } from "../constants/user-constants";
 
-const userSignupSchema = Joi.object({
+export const userSignupSchema = Joi.object({
   userName: Joi.string().required(),
   email: Joi.string().pattern(emailRegex).required(),
   password: Joi.string().min(6).required(),
 });
 
-const userSigninSchema = Joi.object({
+export const userSigninSchema = Joi.object({
   email: Joi.string().pattern(emailRegex).required(),
   password: Joi.string().min(6).required(),
 });
